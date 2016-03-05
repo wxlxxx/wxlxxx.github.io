dc412d47c5d2b8ffe7f57b7e5d89def362747a9e
wxlxxx <964397251@qq.com>
2016-03-06 02:40:39 +0800

    wxl
    xxx

--- js/scroll.js
+++ js/scroll.js
@@ -7,25 +7,40 @@ $(document).ready(function(){
 		$('html,body').animate({scrollTop: '0px'}, 400);
 	});
 	$('#list-group').children().eq(1).click(function(){
-		$('html,body').animate({scrollTop: '700px'}, 400);
+		$('html,body').animate({scrollTop: '2944px'}, 400);
 	});
 	$('#list-group').children().eq(2).click(function(){
-		$('html,body').animate({scrollTop: '2400px'}, 400);
+		$('html,body').animate({scrollTop: '5716px'}, 400);
+	});
+	$('#list-group').children().eq(3).click(function(){
+		$('html,body').animate({scrollTop: '5716px'}, 400);
+	});
+	$('#list-group').children().eq(4).click(function(){
+		$('html,body').animate({scrollTop: '6376px'}, 400);
+	});
+	$('#list-group').children().eq(5).click(function(){
+		$('html,body').animate({scrollTop: '8130px'}, 400);
 	});
 	// $('#list-group').children().eq(3).click(function(){
 	// 	$('html,body').animate({scrollTop: '600px'}, 200);
 	// });
 
 	$(window).scroll(function() {
-		if ($('html,body').scrollTop()<700) {
+		if ($('html,body').scrollTop()<2944) {
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(0).addClass('active');
-		} else if ($('html,body').scrollTop()<2200){
+		} else if ($('html,body').scrollTop()<5716){
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(1).addClass('active');
+		} else if ($('html,body').scrollTop()<6376){
+			$('#list-group a').removeClass('active');
+			$('#list-group').children().eq(3).addClass('active');
+		} else if ($('html,body').scrollTop()<8130){
+			$('#list-group a').removeClass('active');
+			$('#list-group').children().eq(4).addClass('active');
 		} else{
 			$('#list-group a').removeClass('active');
-			$('#list-group').children().eq(2).addClass('active');
+			$('#list-group').children().eq(5).addClass('active');
 		};
 	});
 
