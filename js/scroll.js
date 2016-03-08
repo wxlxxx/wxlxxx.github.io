1837c378c621fe8a8025061b17524409c9186c77
wxlxxx <964397251@qq.com>
2016-03-09 00:38:41 +0800

    wxl

--- js/scroll.js
+++ js/scroll.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+//	alert($('html,body').scrollTop());
 	$('#list-group a').mouseover(function(){
 		$('#list-group a').removeClass('active');
 		$(this).addClass('active');
@@ -7,35 +8,35 @@ $(document).ready(function(){
 		$('html,body').animate({scrollTop: '0px'}, 400);
 	});
 	$('#list-group').children().eq(1).click(function(){
-		$('html,body').animate({scrollTop: '2944px'}, 400);
+		$('html,body').animate({scrollTop: '3444px'}, 400);
 	});
 	$('#list-group').children().eq(2).click(function(){
-		$('html,body').animate({scrollTop: '5716px'}, 400);
+		$('html,body').animate({scrollTop: '6716px'}, 400);
 	});
 	$('#list-group').children().eq(3).click(function(){
-		$('html,body').animate({scrollTop: '5716px'}, 400);
+		$('html,body').animate({scrollTop: '6716px'}, 400);
 	});
 	$('#list-group').children().eq(4).click(function(){
-		$('html,body').animate({scrollTop: '6376px'}, 400);
+		$('html,body').animate({scrollTop: '7376px'}, 400);
 	});
 	$('#list-group').children().eq(5).click(function(){
-		$('html,body').animate({scrollTop: '8130px'}, 400);
+		$('html,body').animate({scrollTop: '9130px'}, 400);
 	});
 	// $('#list-group').children().eq(3).click(function(){
 	// 	$('html,body').animate({scrollTop: '600px'}, 200);
 	// });
 
 	$(window).scroll(function() {
-		if ($('html,body').scrollTop()<2944) {
+		if ($('html,body').scrollTop()<3444) {
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(0).addClass('active');
-		} else if ($('html,body').scrollTop()<5716){
+		} else if ($('html,body').scrollTop()<6716){
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(1).addClass('active');
-		} else if ($('html,body').scrollTop()<6376){
+		} else if ($('html,body').scrollTop()<7376){
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(3).addClass('active');
-		} else if ($('html,body').scrollTop()<8130){
+		} else if ($('html,body').scrollTop()<9130){
 			$('#list-group a').removeClass('active');
 			$('#list-group').children().eq(4).addClass('active');
 		} else{
