dcf5af7bbb3787e46e0da7a6da61713cd4dbde88
wxlxxx <964397251@qq.com>
2016-04-20 20:25:05 +0800

    2024

--- BF_shop/js/index.js
+++ BF_shop/js/index.js
@@ -17,10 +17,34 @@ $(document).ready(function(){
 		min_move_x:100,
 		preventDefaultEvents:true
 	});
+	$('#content-1 .carousel').carousel({
+		interval: 10000
+	});
 	
 	$('#content-2 .size-select button').click(function(){
 		$(this).addClass('active');
 		$(this).siblings().removeClass('active');
 	});
+	
+	
+	
+	
+	if ( $('#content-2').offset().top+20<$('#content-1').height()+$(window).scrollTop() ) {
+		$('#mynavbar').css('background-color','rgba(0,0,0,0.2)');
+	}
+
+	
+	$(window).scroll(function(){
+		
+		
+		if ( $('#content-2').offset().top+20<$('#content-1').height()+$(window).scrollTop() ) {
+			$('#mynavbar').css('background-color','rgba(0,0,0,0.2)');
+		}else{
+			$('#mynavbar').css('background-color','rgba(0,0,0,0)');
+		}
+	
+		
+	});
+
 });
 
