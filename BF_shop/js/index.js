a9c7412a38748a03473ac602f0ac0a6628264d35
wxlxxx <964397251@qq.com>
2016-04-21 16:05:45 +0800

    wxl

--- BF_shop/js/index.js
+++ BF_shop/js/index.js
@@ -1,7 +1,7 @@
 var docEl = document.documentElement,
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
     recalc = function() {
-        docEl.style.fontSize = 100 * (docEl.clientWidth / 750) + 'px';
+        docEl.style.fontSize = parseInt(100 * (docEl.clientWidth / 750)) + 'px';
     };
 window.addEventListener(resizeEvt, recalc, false);
 document.addEventListener('DOMContentLoaded', recalc, false);
