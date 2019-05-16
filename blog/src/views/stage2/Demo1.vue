8b4067802ecb55ea4f52a404ab3983e2908d9c97
wxl <wxl@wxldeiMac.local>
2019-05-16 21:57:14 +0800

    wxl

--- blog/src/views/stage2/Demo1.vue
+++ blog/src/views/stage2/Demo1.vue
@@ -1,14 +1,16 @@
 <template>
 	<div>
 		<h2 class="mb-4">{{currRoute.name}}</h2>
-		<canvas id="demoCanvas" width="500" height="300"></canvas>
+		<elliptical :cw="800" :ch="500"></elliptical>
 	</div>
 </template>
 
 <script>
 	import createjs from 'createjs'
+	import Elliptical from '../../components/Elliptical.vue'
 	export default {
 		name: 'Doc1',
+		components: {Elliptical},
 		data(){
 			return {
 				currRoute: null
@@ -18,47 +20,10 @@
 			this.currRoute = this.$route
 		},
 		mounted(){
-			this.canvasFunc()
+
 		},
 		methods: {
-			canvasFunc(){
-				var stage = new createjs.Stage("demoCanvas");
-		        var circle = new createjs.Shape();
-		        circle.graphics.beginFill("Crimson").drawCircle(0, 0, 50);
-		        circle.x = 100;
-		        circle.y = 100;
-		        stage.addChild(circle);
 
-		        // stage.update();
-
-		        // createjs.Tween.get(circle, {loop: true})
-		        //   .to({x: 400}, 1000, createjs.Ease.getPowInOut(4))
-		        //   .to({alpha: 0, y: 75}, 500, createjs.Ease.getPowInOut(2))
-		        //   .to({alpha: 0, y: 125}, 100)
-		        //   .to({alpha: 1, y: 100}, 500, createjs.Ease.getPowInOut(2))
-		        //   .to({x: 100}, 800, createjs.Ease.getPowInOut(2));
-
-		  //       createjs.Tween.get(circle)
-			 //    .wait(500)
-			 //    .to({alpha:0, visible:false}, 1000)
-			 //    .call(handleComplete);
-			 //    function handleComplete(event) {
-				//     // The tween changed.
-				// }
-
-		        // createjs.Ticker.setFPS(60);
-		        // createjs.Ticker.addEventListener("tick", stage);
-
-		        createjs.Ticker.addEventListener("tick", handleTick);
-
-			    function handleTick() {
-			     //Circle will move 10 units to the right.
-			        circle.x += 2;
-			        //Will cause the circle to wrap back
-			        if (circle.x > stage.canvas.width) { circle.x = 0; }
-			        stage.update();
-			    }
-			}
 		}
 	}
 </script>
--- blog/src/views/stage2/Demo2.vue
+++ blog/src/views/stage2/Demo2.vue
@@ -12,9 +12,10 @@
 		data(){
 			return {
 				currRoute: null,
+
 				stage: null,
 				queue: null,
-				shape: null,
+				shape: [],
 				shapeAngle: 0,
 				C_W: 0,
             	C_H: 0,
@@ -28,17 +29,21 @@
 		},
 		methods: {
 			init(){
+				// 创建queue对象加载图片
 				this.queue = new createjs.LoadQueue();
 				this.queue.on("complete", this.handleComplete, this);
 				this.queue.loadManifest([
 					{id: "bg", src: require('../../assets/img/pic2.png')},
 					{id: "circle", src: require('../../assets/img/icon1.png')}
 				]);
+				// 创建舞台对象
 				this.stage = new createjs.Stage("demoCanvas")
+				// 获取canvas大小
 				this.C_W = this.stage.canvas.width
 	            this.C_H = this.stage.canvas.height
+	            // 舒适化角度变量
 	            this.shapeAngle = Math.PI/1.8+Math.PI*2
-
+	            // 加载中动画
 	            this.drawLoading()
 
 
@@ -76,36 +81,67 @@
 				// }
 			},
 			handleComplete(){
+				// 获取加载好的图片对象
 				var bgImage = this.queue.getResult("bg")
 				var circleImage = this.queue.getResult("circle")
-
+				// 创建舞台背景对象
 				var bg = new createjs.Shape()
 				bg.graphics.bf(bgImage).drawRect(0,0,bgImage.width,bgImage.height)
 				bg.setTransform(0, 0, this.C_W/bgImage.width, this.C_H/bgImage.height)
-
+				// 创建舞台内的其他元素
 				var circle = new createjs.Bitmap(circleImage)
 				circle.setTransform(970,580)
-
-				this.shape = new createjs.Shape()
-
+				this.shape[0] = new createjs.Shape()
+				this.shape[1] = new createjs.Shape()
+				this.shape[2] = new createjs.Shape()
+				this.shape[3] = new createjs.Shape()
+				this.shape[4] = new createjs.Shape()
+				this.shape[1].graphics.beginFill('#000').drawRect(1110, 731, 20, 20)
+				this.shape[2].graphics.beginFill('#000').drawRect(1040, 731, 40, 70)
+				this.shape[3].graphics.beginFill('#000').drawRect(1130, 701, 130, 50)
+				this.shape[4].graphics.beginFill('#000').drawRect(1190, 623, 30, 30)
+				// 添加元素到舞台中
 				this.stage.addChild(bg)
 				this.stage.addChild(circle)
-				this.stage.addChild(this.shape)
-				// this.shape.graphics.setStrokeStyle(20, 'round').beginStroke("#fff").arc(1120, 731, 150, Math.PI/1.8, Math.PI/1.8+Math.PI*2)
+				this.stage.addChild(this.shape[0])
+				this.stage.addChild(this.shape[1])
+				this.stage.addChild(this.shape[2])
+				this.stage.addChild(this.shape[3])
+				this.stage.addChild(this.shape[4])
+				// this.shape[0].graphics.setStrokeStyle(20, 'round').beginStroke("#fff").arc(1120, 731, 150, Math.PI/1.8, Math.PI/1.8+Math.PI*2)
 				this.stage.update()
-
+				// 监听tick事件执行动画
 				createjs.Ticker.addEventListener("tick", this.handleTick)
 				createjs.Ticker.setFPS(60)
 			},
 			handleTick(event){
-				console.log(this.shapeAngle)
 				this.shapeAngle -= Math.PI*2/90
-				this.shape.graphics.clear()
-				this.shape.graphics.setStrokeStyle(20, 'round').beginStroke("#000").arc(1120, 731, 150, Math.PI/1.8, this.shapeAngle)
+				this.shape[0].graphics.clear()
+				this.shape[0].graphics.setStrokeStyle(20, 'round').beginStroke("#000").arc(1120, 731, 150, Math.PI/1.8, this.shapeAngle)
 				if(this.shapeAngle <= Math.PI/1.8){
-					this.shape.graphics.clear()
+					this.shape[0].graphics.clear()
 					createjs.Ticker.removeEventListener("tick", this.handleTick)
+					this.addMark()
 				}
+				this.stage.update()
+			},
+			addMark(){
+				var i = 1
+				var myInterval = setInterval(function(){
+					this.shape[i].graphics.clear()
+					this.stage.update()
+					i += 1
+					if(i >= 5){
+						clearInterval(myInterval)
+						this.addText()
+					}
+				}.bind(this),300)
+			},
+			addText(){
+				var text = new createjs.Text("Hello World", "20px Arial", "#ff7700")
+				text.x = 1190
+				text.y = 623
+				this.stage.addChild(text)
 				this.stage.update()
 			},
 			drawLoading(){
