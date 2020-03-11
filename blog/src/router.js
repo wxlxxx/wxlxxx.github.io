fdb954a06301d51d9ebf45e14090d6edbe8cd4fa
wxl <wxl@SZ17032633C01wangxiaolong.local>
2020-03-11 15:41:34 +0800

    wxl

--- blog/src/router.js
+++ blog/src/router.js
@@ -16,6 +16,7 @@ import Doc7 from './views/stage1/Doc7.vue'
 import Doc8 from './views/stage1/Doc8.vue'
 
 import Stage2List from './views/stage2/Stage2List.vue'
+import Demo1 from './views/stage2/Demo1.vue'
 import Demo3 from './views/stage2/Demo3.vue'
 import Demo4 from './views/stage2/Demo4.vue'
 import Demo5 from './views/stage2/Demo5.vue'
@@ -29,130 +30,126 @@ Vue.use(Router)
 export default new Router({
   // mode: 'history',
   routes: [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+  {
+    path: '/stage1/',
+    name: 'javascript基础',
+    component: Stage1,
+    children: [
     {
-      path: '/',
-      name: 'home',
-      component: Home
+      path: '',
+      name: 'javascript基础',
+      component: Stage1List
     },
     {
-  		path: '/stage1/',
-  		name: 'javascript基础',
-  		component: Stage1,
-      children: [
-        {
-          path: '',
-          name: 'javascript基础',
-          component: Stage1List
-        },
-        {
-          path: 'doc1',
-          name: 'javascript数据类型及检测方法',
-          component: Doc1
-        },
-        {
-          path: 'doc2',
-          name: '原型链',
-          component: Doc2
-        },
-        {
-          path: 'doc3',
-          name: 'AJAX',
-          component: Doc3
-        },
-        {
-          path: 'doc4',
-          name: 'this机制',
-          component: Doc4
-        },
-        {
-          path: 'doc5',
-          name: 'vue数据绑定原理',
-          component: Doc5
-        },
-        {
-          path: 'doc6',
-          name: 'vue虚拟DOM原理',
-          component: Doc6
-        },
-        {
-          path: 'doc7',
-          name: 'javascript面向对象编程',
-          component: Doc7
-        },
-        {
-          path: 'doc8',
-          name: 'es6+ 新特性',
-          component: Doc8
-        },
-      ]
-  	},
-  	{
-  		path: '/stage2/',
-  		name: 'javascript应用',
-  		component: Stage2,
-      children: [
-        {
-          path: '',
-          name: 'javascript应用',
-          component: Stage2List
-        },
-        {
-          path: 'demo3',
-          name: 'Maze',
-          component: Demo3
-        },
-        {
-          path: 'demo4',
-          name: 'Progress loop',
-          component: Demo4
-        },
-        {
-          path: 'demo5',
-          name: 'Progress loop with canvas',
-          component: Demo5
-        },
-        {
-          path: 'demo6',
-          name: 'marquee',
-          component: Demo6
-        },
-        {
-          path: 'demo7',
-          name: 'AnimateDemo',
-          component: Demo7
-        }
-      ]
-  	},
+      path: 'doc1',
+      name: 'javascript数据类型及检测方法',
+      component: Doc1
+    },
     {
-      path: '/stage3/',
+      path: 'doc2',
+      name: '原型链',
+      component: Doc2
+    },
+    {
+      path: 'doc3',
+      name: 'AJAX',
+      component: Doc3
+    },
+    {
+      path: 'doc4',
+      name: 'this机制',
+      component: Doc4
+    },
+    {
+      path: 'doc5',
+      name: 'vue数据绑定原理',
+      component: Doc5
+    },
+    {
+      path: 'doc6',
+      name: 'vue虚拟DOM原理',
+      component: Doc6
+    },
+    {
+      path: 'doc7',
+      name: 'javascript面向对象编程',
+      component: Doc7
+    },
+    {
+      path: 'doc8',
+      name: 'es6+ 新特性',
+      component: Doc8
+    },
+    ]
+  },
+  {
+    path: '/stage2/',
+    name: 'javascript应用',
+    component: Stage2,
+    children: [
+    {
+      path: '',
+      name: 'javascript应用',
+      component: Stage2List
+    },
+    {
+      path: 'demo1',
+      name: 'Colors',
+      component: Demo1
+    },
+    {
+      path: 'demo3',
+      name: 'Maze',
+      component: Demo3
+    },
+    {
+      path: 'demo4',
+      name: 'Progress loop',
+      component: Demo4
+    },
+    {
+      path: 'demo5',
+      name: 'Progress loop with canvas',
+      component: Demo5
+    },
+    {
+      path: 'demo6',
+      name: 'marquee',
+      component: Demo6
+    },
+    {
+      path: 'demo7',
+      name: 'AnimateDemo',
+      component: Demo7
+    }
+    ]
+  },
+  {
+    path: '/stage3/',
+    name: '内部分享',
+    component: Stage3,
+    children: [
+    {
+      path: '',
       name: '内部分享',
-      component: Stage3,
-      children: [
-        {
-          path: '',
-          name: '内部分享',
-          component: Stage3List
-        },
-        {
-          path: 'share1',
-          fullPath: '/share1.html',
-          name: 'A pixel is not a pixel is not a pixel'
-        },
-        {
-          path: 'share2',
-          fullPath: '/share2.html',
-          name: '如何打造一款高质量的web单页应用'
-        }
-      ]
+      component: Stage3List
     },
-
-    // {
-    //   path: '/about',
-    //   name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      //component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
-    //}
+    {
+      path: 'share1',
+      fullPath: '/share1.html',
+      name: 'A pixel is not a pixel is not a pixel'
+    },
+    {
+      path: 'share2',
+      fullPath: '/share2.html',
+      name: '如何打造一款高质量的web单页应用'
+    }
+    ]
+  },
   ]
 })
---
+++ blog/src/views/stage2/Demo1.vue
@@ -0,0 +1,49 @@
+<template>
+	<div>
+		<h2 class="mb-4">{{currRoute.name}}</h2>
+		<div class="row no-gutters">
+			<div class="col" v-for="(color,i) in colors" :key="i">
+				<div class="p-4" :style="{backgroundColor: color}"></div>
+			</div>
+		</div>
+		<div class="py-3">
+			<input class="form-control" v-model="primaryColor" type="text">
+		</div>
+	</div>
+</template>
+
+<script>
+// import { generate } from '@ant-design/colors'
+import generate from '../../assets/js/generateColors.js'
+export default {
+	name: 'Colors',
+	data(){
+		return {
+			currRoute: null,
+
+			colors: [],
+			primaryColor: '#1890ff'
+		}
+	},
+	created(){
+		this.currRoute = this.$route
+
+		this.colors = generate(this.primaryColor)
+	},
+	watch: {
+		primaryColor: function(currVal) {
+			this.colors = generate(currVal)
+		}
+	},
+	mounted(){
+		
+	},
+	methods: {
+		
+	}
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
